e1e77e6db5d96a77e033b8d2b88ce77929376c4a
Samiksha-sys <samikshaaryal03@gmail.com>
2023-10-18 08:06:34 +0545

    wednesday

---
+++ index.js
@@ -0,0 +1,29 @@
+// function shownav() {
+//     // Get elements by class name
+//     var bar = document.getElementsByClassName("fa-bars");
+//     var ull = document.getElementsByClassName("ull");
+    
+
+//    bar.style.display="none";
+//    ull.style.display="flex";
+//     // for (var i = 0; i < xmark.length; i++) {
+        
+       
+//     // }
+// }
+// function hidenav() {
+//     // Get elements by class name
+//     var bar = document.getElementsByClassName("fa-bars");
+//     var xmark = document.getElementsByClassName("fa-xmark");
+
+
+//     for (var i = 0; i < bar.length; i++) {
+//         bar[i].style.display = "block";
+//         xmark[i].style.display = "hidden";
+       
+//     }
+//     // for (var i = 0; i < xmark.length; i++) {
+       
+       
+//     // }
+// }
